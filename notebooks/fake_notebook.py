a39116e4c8230a93c1dc629a523e70ef3a7061cb
KluEvo <30702547+KluEvo@users.noreply.github.com>
2026-02-16 09:09:14 -0500

    notebook stuff

---
+++ notebooks/fake_notebook.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+# Code Block 1
+# Create a sample DataFrame
+data = {    'Name': ['Alice', 'Bob', 'Charlie', 'David', 'Eve'],
+    'Age': [25, 30, 35, 40, 45],                        
+    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix']
+}
+df = pd.DataFrame(data)
+# Display the DataFrame
+print(df)   
+
+print('later')
